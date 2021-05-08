210e283bf01f465330346e18da0dcd3fe8be12a2
hugo@DESKTOP-J7IENLF <cristian260893@gmail.com>
2021-05-08 18:06:26 -0500

    Components

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ listCitas : any[]=[];
+ agregarCita (cita: any){
+   this.listCitas.push(cita);
+ }
+ eliminarCita(index:number){
+    this.listCitas.splice(index,1)
+ }
+}
